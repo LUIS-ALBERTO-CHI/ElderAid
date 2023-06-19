566cd997e04bd8da3a594f1b883ae4a1f21ac5d9
Soufian BOUTAIB <soufian.boutaib@fwa.eu>
2023-06-19 17:28:15 +0200

    Add entity and masterdata for organization

---
+++ FwaEu.MediCare/Organizations/MasterData/UserOrganizationsMasterDataProvider.cs
@@ -0,0 +1,49 @@
+using FwaEu.Fwamework.Data.Database;
+using FwaEu.Fwamework.Data.Database.Sessions;
+using FwaEu.Fwamework.Globalization;
+using FwaEu.Fwamework.Users;
+using FwaEu.Modules.MasterData;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace FwaEu.MediCare.Organizations.MasterData
+{
+	public class UserOrganizationsMasterDataProvider : EntityMasterDataProvider<OrganizationUserLinkEntity, int, UserOrganizationMasterDataModel, OrganizationUserLinkEntityRepository>
+	{
+
+        public UserOrganizationsMasterDataProvider(
+            MainSessionContext sessionContext,
+            ICulturesService culturesService)
+            : base(sessionContext, culturesService)
+        {
+        }
+        protected override Expression<Func<OrganizationUserLinkEntity, UserOrganizationMasterDataModel>> CreateSelectExpression(CultureInfo userCulture, CultureInfo defaultCulture)
+        {
+            return entity => new UserOrganizationMasterDataModel(entity.Id, entity.Organization.InvariantId, entity.Organization.Name, entity.User.Id);
+        }
+        protected override Expression<Func<OrganizationUserLinkEntity, bool>> CreateSearchExpression(string search,
+            CultureInfo userCulture, CultureInfo defaultCulture)
+        {
+            return entity => entity.Organization.Name.Contains(search) || entity.Organization.InvariantId.Contains(search);
+        }
+    }
+
+	public class UserOrganizationMasterDataModel
+    {
+		public UserOrganizationMasterDataModel(int id, string invariantId, string name, int userId)
+		{
+			this.Id = id;
+            this.InvariantId = invariantId;
+			this.Name = name;
+            this.UserId = userId;
+        }
+
+		public int Id { get; }
+		public string InvariantId { get; }
+        public string Name { get; }
+        public int UserId { get; }
+    }
+}
---
+++ FwaEu.MediCare/Organizations/OrganizationsExtensions.cs
@@ -0,0 +1,30 @@
+﻿using FwaEu.Fwamework.Data.Database;
+using FwaEu.Modules.MasterData;
+using Microsoft.Extensions.DependencyInjection;
+
+using FwaEu.Fwamework;
+using FwaEu.MediCare.Organizations.MasterData;
+using FwaEu.Modules.GenericAdmin;
+
+using FwaEu.MediCare.Organizations.GenericAdmin;
+
+namespace FwaEu.MediCare.Organizations
+{
+    public static class OrganizationsExtensions
+    {
+        public static IServiceCollection AddApplicationOrganizations(this IServiceCollection services, ApplicationInitializationContext context)
+        {
+            var repositoryRegister = context.ServiceStore.Get<IRepositoryRegister>();
+            repositoryRegister.Add<OrganizationEntityRepository>();
+            repositoryRegister.Add<OrganizationUserLinkEntityRepository>();
+
+            services.AddMasterDataProvider<OrganizationEntityMasterDataProvider>("Organizations");
+
+            services.AddMasterDataProvider<UserOrganizationsMasterDataProvider>("UserOrganizations");
+
+            services.AddTransient<IGenericAdminModelConfiguration, OrganizationEntityToModelGenericAdminModelConfiguration>();
+
+            return services;
+        }
+    }
+}
