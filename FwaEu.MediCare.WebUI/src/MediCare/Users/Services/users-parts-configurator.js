1f8acf70549fb8f4f0bf2cce6389f5d79b318a5c
David Cardós Uc <david@fwa.eu>
2023-06-27 15:05:40 -0600

    Créer le role Local administrator https://dev.azure.com/fwaeu/MediaCare/_workitems/edit/10495/ Activer la gestion par roles https://dev.azure.com/fwaeu/MediaCare/_workitems/edit/10494/

--- FwaEu.MediCare.WebUI/src/MediCare/Users/Services/users-parts-configurator.js
+++ FwaEu.MediCare.WebUI/src/MediCare/Users/Services/users-parts-configurator.js
@@ -1,6 +1,7 @@
-import CredentialsPart from '@/Modules/DefaultAuthentication/UserParts/Credentials/credentials-user-part';
+//import CredentialsPart from '@/Modules/DefaultAuthentication/UserParts/Credentials/credentials-user-part';
 import CulturePart from '@/Modules/UserCulture/Components/culture-user-part';
 import HistoryPart from '@/Modules/UserHistory/Components/history-user-part';
+import RolePart from '@/Modules/Roles/Components/role-user-part';
 import AdminStatePart from '@/Modules/UserAdminState/Components/admin-state-user-part';
 import UserPartsRegistry from '@/Fwamework/Users/Services/users-parts-registry';
 import UserApplicationUserPart from '@/MediCare/Users/Services/user-application-user-part';
@@ -15,6 +16,7 @@ export default {
 		UserPartsRegistry.addUserPart(HistoryPart);
 		UserPartsRegistry.addUserPart(AdminStatePart);
 		UserPartsRegistry.addUserPart(UserApplicationUserPart);
+		UserPartsRegistry.addUserPart(RolePart);
 		UserPartsRegistry.addUserPart(OrganizationUserPart);
 	}
 }
