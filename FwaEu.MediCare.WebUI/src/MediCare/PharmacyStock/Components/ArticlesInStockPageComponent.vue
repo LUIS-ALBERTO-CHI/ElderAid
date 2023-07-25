95e33810cf4ee1b9c59a954168a0da7dd3daf85b
LUIS-ALBERTO-CHI <105991202+LUIS-ALBERTO-CHI@users.noreply.github.com>
2023-07-24 22:39:02 -0600

    there are some things to resolve

--- FwaEu.MediCare.WebUI/src/MediCare/PharmacyStock/Components/ArticlesInStockPageComponent.vue
+++ FwaEu.MediCare.WebUI/src/MediCare/PharmacyStock/Components/ArticlesInStockPageComponent.vue
@@ -10,19 +10,21 @@
                 <i @click="goToScanCode" class="fa-sharp fa-regular fa-qrcode qr-code-icon"></i>
             </span>
             <div class="vignette-list">
-                <div v-for="article in filteredArticles" :key="article.name">
+                <div v-for="article in filteredArticles" :key="article.id">
                     <div @click="goToArticleDetails(article)" class="vignette-item">
-                        <span>{{ article.name }}, {{ article.unit }}</span>
-                        <span>{{ article.countInbox }}</span>
+                        <span>{{ article.title }}, {{ article.unit }}</span>
+                        <span>{{ article.countInBox }}</span>
                     </div>
                 </div>
+                <span @click="loadMoreArticlesAsync" class="load-more-text">Plus
+                d'articles</span>
             </div>
             <div v-show="filteredArticles.length === 0" class="article-not-found">
                 <i class="fa-solid fa-box-open icon-not-found"></i>
                 <span>Aucun article trouvé</span>
             </div>
         </div>
-        <div v-show="showScanner">
+        <div v-if="showScanner">
             <ScannerComponent @codeScanned="handleCodeScanned" @cancelScan="handleCancelScan"></ScannerComponent>
         </div>
     </div>
@@ -30,19 +32,59 @@
 
 <script>
 import InputText from 'primevue/inputtext';
-import articles from './articles.json';
 import CabinetsMasterDataService from "@/MediCare/Referencials/Services/cabinets-master-data-service";
 import ScannerComponent from '@/MediCare/Components/ScanCodeComponent.vue';
+import ArticlesInStockService from '@/MediCare/PharmacyStock/Services/search-articles-in-stock-service';
+import { useRoute } from 'vue-router';
+import { ref, watch } from "vue";
+import { watchDebounced } from '@vueuse/core'
+import OnlineService from '@/fwamework/OnlineStatus/Services/online-service';
+import NotificationService from '@/Fwamework/Notifications/Services/notification-service';
 
 export default {
     components: {
         InputText,
         ScannerComponent
     },
+        setup() {
+        const route = useRoute();
+        const searchValue = ref(route.query.searchMode ?? "");
+        const filteredArticles = ref([]);
+        const articles = ref([]);
+        const cabinet = ref();
+        const currentPage = ref(0);
+        const nextPage = ref(0);
+        const pageSize = ref(30);
+        const performSearch = async () => {
+            const value = searchValue.value.toLowerCase().trim();
+            if (!value) {
+                filteredArticles.value = articles.value;
+            } else if (value.length >= 3) {
+                const response = await ArticlesInStockService.getAllAsync(cabinet.value, searchValue.value, nextPage.value, pageSize.value);
+                filteredArticles.value = response.articles;
+            } else {
+                filteredArticles.value = [];
+            }
+        };
+
+        const watchSearchValue = watchDebounced([searchValue], performSearch, { debounce: 500 });
+        const watchResetPage = watch([searchValue], () => currentPage.value == 0);
+
+        return {
+            searchValue,
+            filteredArticles,
+            watchSearchValue,
+            articles,
+            performSearch,
+            currentPage,
+            watchResetPage,
+            nextPage,
+            pageSize,
+            cabinet
+        };
+    },
     data() {
         return {
-            articles: articles,
-            searchValue: "",
             cabinetName: '',
             showScanner: false,
         };
@@ -52,6 +94,15 @@ export default {
         await this.getCurrentCabinetAsync();
     },
     methods: {
+            async loadMoreArticlesAsync() {
+            if (OnlineService.isOnline()) {
+                const nextPage = this.currentPage + 1;
+                this.currentPage = nextPage;
+                this.performSearch();
+            } else {
+                NotificationService.showError("La connexion avec le serveur a été perdue. Retentez plus tard")
+            }
+        },
         removeSearch() {
             this.searchValue = "";
             this.focusSearchBar();
@@ -81,18 +132,11 @@ export default {
         handleCancelScan() {
             this.showScanner = false;
         },
+        loadInitialArticles() {
+            this.performSearch();
+        },
     },
     computed: {
-        filteredArticles() {
-            const searchValue = this.searchValue.toLowerCase().trim();
-            if (!searchValue) {
-                return this.articles;
-            } else {
-                return this.articles.filter(article =>
-                    article.name.toLowerCase().includes(searchValue)
-                );
-            }
-        },
         showPage() {
             return !this.showScanner
         }
