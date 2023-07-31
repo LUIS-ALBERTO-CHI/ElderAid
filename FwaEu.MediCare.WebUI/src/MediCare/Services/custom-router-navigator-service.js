edd8554df1cf8ac19b3c0630fe765b7d8a236e07
Rodrigues mathis <mathis.rodrigues@fwa.eu>
2023-07-31 16:20:45 +0200

    fix meta title in admin pages

--- FwaEu.MediCare.WebUI/src/MediCare/Services/custom-router-navigator-service.js
+++ FwaEu.MediCare.WebUI/src/MediCare/Services/custom-router-navigator-service.js
@@ -3,7 +3,10 @@ import Router from '@/Fwamework/Routing/Services/vue-router-service';
 export default {
 	async configureAsync() {
         Router.afterEach((route) => {
-            document.title = route.meta.title;
+            if (route.name == "default")
+                document.title = route.meta.title;
+            else
+                document.title = `Administration - ${route.name}`
         });
 	}
 }
